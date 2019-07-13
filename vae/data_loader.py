919261c75bbdc5ce28fbb05db13cd1ff0b1f0da1
Gli <43455032+gli-27@users.noreply.github.com>
2019-07-13 09:21:42 -0400

    Add files via upload

--- vae/data_loader.py
+++ vae/data_loader.py
@@ -1,7 +1,41 @@
 import torch
-import torchvision
+from torchvision import transforms
 import torchvision.datasets as datasets
+from torch.utils.data import Dataset
 
-mnist_trainset = datasets.MNIST(root='./data', train=True, download=True, transform=None)
-mnist_testset = datasets.MNIST(root='./data', train=False, download=True, transform=None)
-print(mnist_trainset)
+class DataLoader(Dataset):
+    def __init__(self, opt):
+        self.opt = opt
+        self.dataset = createDataset(opt)
+        self.dataloader = torch.utils.data.DataLoader(
+            self.dataset,
+            batch_size=opt.batch_size,
+            shuffle=not opt.serial_batches,
+            num_workers=int(opt.nThreads)
+        )
+
+    def load_data(self):
+        return self.dataloader
+
+    def __len__(self):
+        return len(self.dataset)
+
+    def name(self):
+        return 'MNIST'
+
+def createDataLoader(opt):
+    data_loader = DataLoader(opt)
+    print(data_loader.name())
+    return data_loader
+
+def createDataset(opt):
+    if opt.isTrain:
+        dataset = datasets.MNIST(root="./data/",
+                                transform=transforms,
+                                train=True,
+                                download=True)
+    else:
+        dataset = datasets.MNIST(root="./data/",
+                                   transform=transforms,
+                                   train=False)
+    return dataset
