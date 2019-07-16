7bc6d108220a44c17b700e2ee255d6ea1050cd90
Gli <43455032+gli-27@users.noreply.github.com>
2019-07-15 23:31:33 -0400

    Update model.py

--- vae/model.py
+++ vae/model.py
@@ -1,10 +1,10 @@
 import torch
 import network
+import torch.nn as nn
 
 def create_model(opt):
     if opt.isTrain:
         model = TrainModel(opt)
-
     return model
 
 class TrainModel(torch.nn.Module):
@@ -16,4 +16,14 @@ class TrainModel(torch.nn.Module):
         self.isTrain = opt.isTrain
         if self.isTrain:
             self.netE = network.Encoder(opt.input_nc, opt.ngf, opt.n_downsampling)
+            self.netE.apply(network.weights_init)
             self.netG = network.Decoder(opt.input_nc, opt.output_nc, opt.ngf, opt.n_downsampling)
+            self.netG.apply(network.weights_init)
+            self.netD = network.Discriminator(opt.input_nc, opt.ngf, opt.n_layer)
+            self.netD.apply(network.weights_init)
+            self.criterionGAN = nn.BCELoss()
+            self.criterionKL = network.KLLoss
+            self.criterionRecon = network.ReconLoss
+        else:
+            pass
+
